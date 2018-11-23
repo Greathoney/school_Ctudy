b00f46cb57230239dd6e0eb82f752d015e6e03c8
Dohun Kim <44798682+yh08037@users.noreply.github.com>
2018-11-23 12:53:09 +0900

    Add files via upload

---
+++ week1/김도훈(yh08037)의 답안/문제 3-2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void){
+	int i=0, turn;
+	char input, tsy[] = {'t', 's', 'y'};
+	
+	turn = 1; //선공을 입력받습니다 
+	
+	//turn = 0; //후공을 입력 받습니다 
+	
+	while(1){
+		if(turn){
+			printf("재원 : ");
+			scanf(" %c", &input);
+			if(input!=tsy[i%3]){
+				printf("YOU LOSE\n");
+				break;
+			}
+			turn = 0;
+		} 
+		else{
+			if(i>=30){
+				printf("대헌 : 기권\n");
+				break;
+			}
+			printf("대헌 : %c\n", tsy[i%3]);
+			turn = 1;
+		}
+		i++;
+		
+	}
+	
+	return 0;
+}
